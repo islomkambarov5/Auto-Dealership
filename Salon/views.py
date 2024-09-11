c49b9b02ca85fe5278d7e5add4509dc204cf264f
islomkambarov5 <islomkambarov5@gmail.com>
2024-09-11 19:05:37 +0500

    New view function and some bug fixings

--- Salon/views.py
+++ Salon/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import *
 
@@ -12,5 +12,19 @@ class CarsList(ListView):
     context_object_name = 'cars'
     extra_context = {
         'title': 'Auto Dealership',
-        'dachas': Cars.objects.all()
+        'cars': Cars.objects.all()
     }
+
+
+class CarDetail(DetailView):
+    model = Cars
+    template_name = 'salon/index.html'
+    context_object_name = 'car_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        car = Cars.objects.get(slug=self.kwargs['slug'])
+
+        context['title'] = 'Car Model Detail - Auto Dealership'
+        context['car'] = car
+
