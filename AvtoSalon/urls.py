4b5e4ac5e25cede88b0debb26a1e39268f33146c
islomkambarov5 <islomkambarov5@gmail.com>
2024-09-11 17:35:23 +0500

    setting up 'urls.py'

--- AvtoSalon/urls.py
+++ AvtoSalon/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Salon.urls'))
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+
